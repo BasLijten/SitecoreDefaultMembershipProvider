e3a09331a6a82558d215474c1a544281439d90ed
Bas Lijten <bas.lijten@gmail.com>
2016-03-30 17:14:23 +0200

    a

---
+++ ResetPassword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace ResetPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encpwd = "5J1irmSbXoEVWMQA0rxa8T3/CZJUV+7rov1Yh5D+vL4=";
+            byte[] bpasswd = Convert.FromBase64String(encpwd);
+
+            string userId = @"sitecore\admin";
+            string passwd = String.Empty;            
+            MembershipUser user = Membership.GetUser(userId, false);
+            passwd = user.ResetPassword();
+            Console.WriteLine(passwd);
+            Console.ReadLine();
+        }
+    }
+}
