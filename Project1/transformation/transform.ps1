e6d5ca286e41787c2d9a317334410d62385ca079
Bas Lijten <bas.lijten@gmail.com>
2016-04-29 16:02:09 +0200

    added webconfig transformation

---
+++ Project1/transformation/transform.ps1
@@ -0,0 +1,28 @@
+function XmlDocTransform($xml, $xdt)
+{
+    if (!$xml -or !(Test-Path -path $xml -PathType Leaf)) {
+        throw "File not found. $xml";
+    }
+    if (!$xdt -or !(Test-Path -path $xdt -PathType Leaf)) {
+        throw "File not found. $xdt";
+    }
+ 
+    $scriptPath = $pwd #(Get-Variable MyInvocation -Scope 1).Value.InvocationName | split-path -parent
+    Add-Type -LiteralPath ".\Microsoft.Web.XmlTransform.dll"
+ 
+    $xmldoc = New-Object Microsoft.Web.XmlTransform.XmlTransformableDocument;
+    $xmldoc.PreserveWhitespace = $true
+    $xmldoc.Load($xml);
+ 
+    $transf = New-Object Microsoft.Web.XmlTransform.XmlTransformation($xdt);
+    if ($transf.Apply($xmldoc) -eq $false)
+    {
+        throw "Transformation failed."
+    }
+    $xmldoc.Save($xml);
+}
+ 
+XmlDocTransform .\Web.config .\ChangeProvider.xdt
+
+ 
+ 
--- ResetPassword/Program.cs
+++ ResetPassword/Program.cs
@@ -11,9 +11,8 @@ namespace ResetPassword
     {
         static void Main(string[] args)
         {
-            string encpwd = "5J1irmSbXoEVWMQA0rxa8T3/CZJUV+7rov1Yh5D+vL4=";
-            byte[] bpasswd = Convert.FromBase64String(encpwd);
-
+            System.Security.Cryptography.CryptoConfig.AddAlgorithm(typeof(Zetetic.Security.Pbkdf2Hash), "pbkdf2_local");
+           
             string userId = @"sitecore\admin";
             string passwd = String.Empty;            
             MembershipUser user = Membership.GetUser(userId, false);
